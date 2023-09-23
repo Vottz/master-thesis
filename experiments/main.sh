8c54fe4868a35aba14518591157c7c0611d706c9
vottz <kevin.parkkreutz@gmail.com>
2023-09-23 16:18:53 +0200

    updated main.sh

--- experiments/main.sh
+++ experiments/main.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 python3 1optimization.py
 python3 2baseline.py
-
-python3 4simpleCA.py
-python3 5randomEB.py
-python3 6leftoverCA.py
+python3 3simpleCA.py
+python3 4randomEB.py
+python3 5leftoverCA.py
+python3 6leftoverCA_addToEB.py
 python3 7leftoverWeightedBudget.py
-python3 8leftoverCA_addToEB.py
